dd799c8db95eae1d4f83e1c8e6d4b9e894b469d9
alvaro <alvarovillegasmontero6@gmail.com>
2026-09-15 00:33:31 +0200

    Add ReservaForm component to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import Services from "./components/Services"
+import ReservaForm from "./components/ReservaForm"
 import Footer from "./components/Footer"
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <Hero />
       <Services />
+      <ReservaForm />
       <Footer />
     </>
   )
